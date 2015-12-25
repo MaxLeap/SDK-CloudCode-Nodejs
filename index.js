420413186fdc472e8d1c22e90a074b661d9bc396
czhou <czhou@ilegendsoft.com>
2015-12-25 10:59:28 +0800

    fixbug

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var ML = require('./lib/ml'),
-    http = require('http');
 
+var ML = require('./lib/ml');
 module.exports = ML;
-ML.initialize('566fc8b1e519c20001a6d51c','RkdXQ1ZYeXBjS1k5dEJ5cUJUWmF2UQ','THJEaUZfbmVjaFJ6eFU4dXF6T01NUQ');
-ML.serverURL = 'http://apiuat.maxleap.cn/';
-http.createServer(ML.Cloud).listen(3000)
